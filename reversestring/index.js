181cdeec47ef5a8dcd33bb62336f2f876906375d
Randall <108246257+randalltr@users.noreply.github.com>
2022-09-14 08:13:39 +0700

    solved string reversal with decrementing for loop

--- reversestring/index.js
+++ reversestring/index.js
@@ -7,7 +7,15 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-    return str.split("").reverse().join("");
+    let reverseString = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        reverseString += str[i];
+    }
+    return reverseString;
 }
 
 module.exports = reverse;
+
+// function reverse(str) {
+//     return str.split("").reverse().join("");
+// }
