5ae649bbb96c818607f07b5c61d498356ba5d4a2
Randall <108246257+randalltr@users.noreply.github.com>
2022-09-22 10:38:39 +0700

    alternative more elegant solution to chunk problem using index and slice

--- chunk/index.js
+++ chunk/index.js
@@ -8,24 +8,35 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(unchunked, size) {
-    // create empty array to hold chunks called 'chunked'
+function chunk(array, size) {
     const chunked = [];
-    // for each element in 'unchunked' array
-    for (let element of unchunked) {
-        const last = chunked[chunked.length - 1];
-    // retrieve last element in 'chunked'
-        if (!last || last.length === size) {
-    // if last element does not exist or if length equal to chunk size
-            chunked.push([element]);
-    // push a new chunk into 'chunked' with the current element
-        } else {
-    // else add the current element into the chunk
-            last.push(element);
-    } }
+    let index = 0;
+    while (index < array.length) {
+        chunked.push(array.slice(index, index + size))
+        index = index + size;
+    }
     return chunked;
 }
 
-console.log(chunk([1, 2, 3, 4], 2))
+console.log(chunk([1, 2, 3, 4], 2));
+console.log(chunk([1, 2, 3, 4, 5, 6, 7, 8], 3));
 
 module.exports = chunk;
+
+// function chunk(unchunked, size) {
+//     // create empty array to hold chunks called 'chunked'
+//     const chunked = [];
+//     // for each element in 'unchunked' array
+//     for (let element of unchunked) {
+//         const last = chunked[chunked.length - 1];
+//     // retrieve last element in 'chunked'
+//         if (!last || last.length === size) {
+//     // if last element does not exist or if length equal to chunk size
+//             chunked.push([element]);
+//     // push a new chunk into 'chunked' with the current element
+//         } else {
+//     // else add the current element into the chunk
+//             last.push(element);
+//     } }
+//     return chunked;
+// }
