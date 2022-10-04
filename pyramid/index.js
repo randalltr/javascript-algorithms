4625371a091f9a61f247e1db61e1dffac7d60f34
Randall <108246257+randalltr@users.noreply.github.com>
2022-10-04 09:44:50 +0700

    iterative solution for pyramid problem using Math.floor

--- pyramid/index.js
+++ pyramid/index.js
@@ -14,6 +14,39 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+
+function pyramid(n) {
+    const midpoint = Math.floor((2 * n - 1) / 2);
+
+    for (let row = 0; row < n; row++) {
+        level = '';
+
+        for (let col = 0; col < 2 * n - 1; col++) {
+            if (midpoint - row <= col && midpoint + row >= col) {
+                level += '#';
+            } else {
+                level += ' ';
+            }
+        }
+        console.log(level);
+    }
+}
+
+pyramid(3);
 
 module.exports = pyramid;
+
+// function steps(n) {
+//     for (i = 0; i < n; i++) {
+//         stair = "";
+//         for (j = 0; j < n; j++) {
+//             if (j <= i) {
+//                 stair += "#";
+//             } else {
+//                 stair += " ";
+//             }
+//         }
+//         console.log(stair);
+//     }
+// }
+
