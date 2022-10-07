acccbd3e1ee90f819a989c49a39d19d68916c876
Randall <108246257+randalltr@users.noreply.github.com>
2022-10-07 15:07:45 +0700

    solved vowels problem

--- vowels/index.js
+++ vowels/index.js
@@ -7,6 +7,17 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+    count = 0;
+
+    for (let letter of str) {
+        if (letter === 'a' || letter === 'e' || letter === 'i' || letter === 'o' || letter === 'u') {
+            count += 1;
+        }
+    }
+    return count;
+}
+
+console.log(vowels('Why?'));
 
 module.exports = vowels;
