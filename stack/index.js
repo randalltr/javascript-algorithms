35d26862a88928a7e83bc9036379509fb7f10777
Randall <108246257+randalltr@users.noreply.github.com>
2022-11-23 09:00:46 +0700

    solved stack exercise

--- stack/index.js
+++ stack/index.js
@@ -10,6 +10,22 @@
 //   s.pop(); // returns 2
 //   s.pop(); // returns 1
 
-class Stack {}
+class Stack {
+    constructor() {
+        this.data = [];
+      }
+    
+      push(record) {
+        this.data.push(record);
+      }
+    
+      peek() {
+        return this.data[this.data.length - 1];
+      }
+    
+      pop() {
+        return this.data.pop();
+      }
+}
 
 module.exports = Stack;
